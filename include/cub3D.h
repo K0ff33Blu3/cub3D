5e2572a9df87622c831fec62bfe1b511cdf15d59
Elmond0 <elmondo1993@gmail.com>
2026-01-17 12:07:38 +0100

    include

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: elmondo <elmondo@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 11:49:50 by elmondo           #+#    #+#             */
-/*   Updated: 2026/01/17 11:53:52 by elmondo          ###   ########.fr       */
+/*   Updated: 2026/01/17 12:06:59 by elmondo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,30 @@
 
 // INCLUDE
 
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
+# include <sys/time.h>
+# include <X11/X.h>
+# include "libft/libft.h"
+# include "minilibx-linux/mlx.h"
+# include "minilibx-linux/mlx_int.h"
+
+// KEYBOARD CODES
+
+#define KB_W 119
+#define KB_S 115
+#define KB_A 97
+#define KB_D 100
+#define KB_ESC 65307
+#define KB_UP 65362
+#define KB_DOWN 65364
+#define KB_LEFT 65361
+#define KB_RIGHT 65363
+#define LSHIFT 65505
+#define LALT 65513
 
 #endif
