9acf5ac47a12c36685f43628933482b12cd14b3b
k0ff33Blu3 <milva.ricci@gmail.com>
2026-01-17 16:23:58 +0100

    mergiato con live share

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,12 +6,14 @@
 /*   By: miricci <miricci@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 11:49:50 by elmondo           #+#    #+#             */
-/*   Updated: 2026/01/17 16:14:43 by miricci          ###   ########.fr       */
+/*   Updated: 2026/01/17 16:21:55 by miricci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
+
+# define MAP_ALLOWED "01NSEW \n"
 
 // SCREEN
 
@@ -46,6 +48,33 @@
 # include "libft/libft.h"
 # include "minilibx-linux/mlx.h"
 # include "minilibx-linux/mlx_int.h"
+
+// ERROR
+
+#define ERR_ARGS "	incorrect arguments number"
+#define ERR_FORMAT "	incorrect file format"
+#define ERR_OPEN "	failed to open path"
+#define ERR_EMPTY_OR_FOLDER "	path points to empty file or folder"
+#define ERR_WALL_REPEAT "	wall is repeated in scene description file"
+#define ERR_FC_REPEAT "	floor or ceiling is repeated \
+in scene description file"
+#define ERR_FC_MISS "	floor or ceiling is missing in scene description file"
+#define ERR_WALL_MISS "	wall is missing in scene description file"
+#define ERR_FC_FORMAT "	color format invalid"
+#define ERR_FC_BOUNDS "	color value out of bounds"
+#define ERR_NEWLINE_MAP "	empty line in map"
+#define ERR_OPEN_MAP "	open map"
+#define ERR_NO_MAP "	missing map"
+#define ERR_PATH_WALL "	wall path incorrect"
+#define ERR_SPACE_END_PATH "	trim chars at end of wall path"
+#define ERR_SPAWN "	map needs exactly ONE of the following chars: N S W E"
+#define BAD_CHAR "	invalid character"
+#define ERR_CHAR_FILE "	invalid character in scene description file"
+#define MALLOC "	malloc failure"
+#define ERR_ENVP " missing env variable(s)"
+#define ERR_ONLINE "online setup failed"
+#define ERR_IP_FORMAT "wrong IP format"
+#define ERR_MALLOC "malloc failure"
 
 typedef	struct s_game
 {
