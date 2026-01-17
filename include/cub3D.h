6f569d98016ee2c511466fc2e4789abf7903fe34
Elmond0 <elmondo1993@gmail.com>
2026-01-17 16:54:34 +0100

    include

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: elmondo <elmondo@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 11:49:50 by elmondo           #+#    #+#             */
-/*   Updated: 2026/01/17 16:43:47 by elmondo          ###   ########.fr       */
+/*   Updated: 2026/01/17 16:53:51 by elmondo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,37 @@
 # define WID	600
 # define LEN	400
 
-// KEYLOG
+#define MAP_ALLOWED "01NSEW \n"
 
-#define W 119
-#define S 115
-#define A 97
-#define D 100
-#define ESC 65307
-#define UP 65362
-#define DOWN 65364
-#define LEFT 65361
-#define RIGHT 65363
-#define LSHIFT 65505
-#define LALT 65513
+// array walls images
+
+#define NO 0
+#define SO 1
+#define EA 2
+#define WE 3
+
+// ERRORS
+
+#define ERR_ARGS "	incorrect arguments number"
+#define ERR_FORMAT "	incorrect file format"
+#define ERR_OPEN "	failed to open path"
+#define ERR_FC_MISS "	floor or ceiling is missing in scene description file"
+#define ERR_WALL_MISS "	wall is missing in scene description file"
+#define ERR_FC_FORMAT "	color format invalid"
+#define ERR_FC_BOUNDS "	color value out of bounds"
+#define ERR_NEWLINE_MAP "	empty line in map"
+#define ERR_OPEN_MAP "	open map"
+#define ERR_NO_MAP "	missing map"
+#define ERR_PATH_WALL "	wall path incorrect"
+#define ERR_SPACE_END_PATH "	trim chars at end of wall path"
+#define ERR_SPAWN "	map needs exactly ONE of the following chars: N S W E"
+#define BAD_CHAR "	invalid character"
+#define ERR_CHAR_FILE "	invalid character in scene description file"
+#define MALLOC "	malloc failure"
+#define ERR_ENVP " missing env variable(s)"
+#define ERR_MALLOC "malloc failure"
+
+
 // SCREEN
 
 # define WID	600
